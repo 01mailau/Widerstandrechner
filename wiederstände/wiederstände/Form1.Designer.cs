1498266e6bff442783d6d19080f7f0f5b0793ea5
01mailau <01mailau@rgtfo-me>
2018-03-02 12:34:00 +0100

    nix

---
+++ wiederstände/wiederstände/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace wiederstände
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Ausgeben = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "schwar",
+            "braun",
+            "rot",
+            "orange",
+            "gelb",
+            "grün",
+            "blau",
+            "violett",
+            "grau weiß"});
+            this.comboBox1.Location = new System.Drawing.Point(180, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "1.Farbring";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "schwar",
+            "braun",
+            "rot",
+            "orange",
+            "gelb",
+            "grün",
+            "blau",
+            "violett",
+            "grau weiß"});
+            this.comboBox2.Location = new System.Drawing.Point(307, 21);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "2.Farbring";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "schwar",
+            "braun",
+            "rot",
+            "orange",
+            "gelb",
+            "grün",
+            "blau",
+            "violett",
+            "grau weiß"});
+            this.comboBox3.Location = new System.Drawing.Point(434, 21);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.Text = "3.Farbring";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.AutoCompleteCustomSource.AddRange(new string[] {
+            "silber",
+            "gold",
+            "rot",
+            "braun",
+            "grün",
+            "blau",
+            "violett"});
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "silber",
+            "gold",
+            "rot",
+            "braun "});
+            this.comboBox4.Location = new System.Drawing.Point(561, 21);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.Text = "4.Toleranz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(407, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Ausgeben
+            // 
+            this.Ausgeben.Location = new System.Drawing.Point(180, 88);
+            this.Ausgeben.Name = "Ausgeben";
+            this.Ausgeben.Size = new System.Drawing.Size(75, 23);
+            this.Ausgeben.TabIndex = 5;
+            this.Ausgeben.Text = "Ausgeben";
+            this.Ausgeben.UseVisualStyleBackColor = true;
+            this.Ausgeben.Click += new System.EventHandler(this.Ausgeben_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 261);
+            this.Controls.Add(this.Ausgeben);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Ausgeben;
+    }
+}
+
---
+++ wiederstände/wiederstände/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace wiederstände
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem.ToString();
+            rechnung();
+            ringe();
+
+
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.SelectedItem.ToString();
+            rechnung();
+            ringe1();
+
+
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rechnung();
+            ringe2();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ringe()
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                string schwarz =" 0";
+            }
+
+            if (comboBox1.SelectedIndex == 1)
+            {
+                string braun = "1";
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                string rot = "2";
+            }
+            if (comboBox1.SelectedIndex == 3)
+            {
+                string orange = "3";
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                string gelb = "4";
+            }
+            if (comboBox1.SelectedIndex == 5)
+            {
+                string grün  = "5";
+            }
+            if (comboBox1.SelectedIndex == 6)
+            {
+                string blau = "6";
+            }
+            if (comboBox1.SelectedIndex == 7)
+            {
+                string violett = "7";
+            }
+            if (comboBox1.SelectedIndex == 8)
+            {
+                string grau = "8";
+            }
+            if (comboBox1.SelectedIndex == 9)
+            {
+                string weiß= "9";
+            }
+
+        }
+
+           private void ringe1()
+           {
+
+            if (comboBox2.SelectedIndex == 0)
+            {
+                string schwarz = "0";
+            }
+
+            if (comboBox2.SelectedIndex == 1)
+            {
+                string braun = "1";
+            }
+            if (comboBox2.SelectedIndex == 2)
+            {
+                string rot = "2";
+            }
+            if (comboBox2.SelectedIndex == 3)
+            {
+                string orange = "3";
+            }
+            if (comboBox2.SelectedIndex == 4)
+            {
+                string gelb = "4";
+            }
+            if (comboBox2.SelectedIndex == 5)
+            {
+                string grün = "5";
+            }
+            if (comboBox2.SelectedIndex == 6)
+            {
+                string blau = "6";
+            }
+            if (comboBox2.SelectedIndex == 7)
+            {
+                string violett = "7";
+            }
+            if (comboBox2.SelectedIndex == 8)
+            {
+                string grau = "8";
+            }
+            if (comboBox2.SelectedIndex == 9)
+            {
+                string weiß = "9";
+            }
+
+        }
+
+           private void ringe2()
+           {
+
+               if (comboBox3.SelectedIndex == 0)
+               {
+                   int schwarz = 0;
+               }
+
+               if (comboBox3.SelectedIndex == 1)
+               {
+                   int braun = 1;
+               }
+               if (comboBox3.SelectedIndex == 2)
+               {
+                   int rot = 2;
+               }
+               if (comboBox3.SelectedIndex == 3)
+               {
+                   int orange = 3;
+               }
+               if (comboBox3.SelectedIndex == 4)
+               {
+                   int gelb = 4;
+
+                   if (comboBox3.SelectedIndex == 5)
+                   {
+                       int grün = 5;
+                   }
+                   if (comboBox3.SelectedIndex == 6)
+                   {
+                       int blau = 6;
+                   }
+                   if (comboBox3.SelectedIndex == 7)
+                   {
+                       int violett = 7;
+                   }
+                   if (comboBox3.SelectedIndex == 8)
+                   {
+                       int grau = 8;
+                   }
+                   if (comboBox3.SelectedIndex == 9)
+                   {
+                       int weiß = 9;
+                   }
+
+               }
+           }
+           private void ringe3()
+           {
+
+               if (comboBox3.SelectedIndex == 0)
+               {
+                   int silber= 10;
+               }
+
+               if (comboBox3.SelectedIndex == 1)
+               {
+                   int gold = 5;
+               }
+               if (comboBox3.SelectedIndex == 2)
+               {
+                   int rot = 1;
+               }
+               if (comboBox3.SelectedIndex == 3)
+               {
+                   int braun = 2;
+               }
+               
+           }
+        private void rechnung()
+        {
+ 
+        }
+
+        private void Ausgeben_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedIndex.ToString();
+            textBox1.Text += comboBox2.SelectedIndex.ToString();
+            textBox1.Text += comboBox3.SelectedIndex.ToString();
+        
+        }
+    }
+}
